5e741bd161472644d3ae1207a656bcd1ff9351ec
Tony <Tony@Tony-PC>
2013-07-03 01:35:19 +0800

    解析JSON的版本

--- src/com/dbconnection/MainActivity.java
+++ src/com/dbconnection/MainActivity.java
@@ -1,6 +1,8 @@
 package com.dbconnection;
 
+
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.app.Activity;
 import android.view.*;
 import android.view.View.OnClickListener;
@@ -9,19 +11,30 @@ import org.apache.http.*;
 import org.apache.http.client.*;
 import org.apache.http.client.methods.*;
 import org.apache.http.impl.client.*;
-import org.apache.http.util.*;
+import org.apache.http.params.CoreConnectionPNames;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.*;
+//import java.net.*;
 
 
 
-
+	
 public class MainActivity extends Activity implements OnClickListener {
 	
 	private TextView mesg;	
 	private Button connectButton;
 	private InputStreamReader input;
 	private BufferedReader reafBuf;
+	private String str2;
+	
+	private HttpClient client;
+	private HttpPost request;
+	private HttpResponse response;
+	private HttpEntity entity;
+	private JSONObject jsonString;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +42,21 @@ public class MainActivity extends Activity implements OnClickListener {
 		setContentView(R.layout.activity_main);
 		
 		mesg = (TextView)findViewById(R.id.mesgTextView);
+		
 		connectButton = (Button)findViewById(R.id.connectButton);
+		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+	    .detectDiskReads()
+	    .detectDiskWrites()
+	    .detectNetwork()   // or .detectAll() for all detectable problems
+	    .penaltyLog()
+	    .build());
+		
+		StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()  
+        .detectLeakedSqlLiteObjects()   
+        .penaltyLog()  
+        .penaltyDeath()  
+        .build());  
+		
 		connectButton.setOnClickListener(this);
 	}
 
@@ -43,33 +70,49 @@ public class MainActivity extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View v) 
 	{
-		mesg.setText(stringQuery("http://localhost/DBCon.php"));
+		stringQuery("http://118.169.189.73/DBcon.php");
 	}
 	
-	private String stringQuery(String url)
+	private void stringQuery(String url)
 	{
+		client = new DefaultHttpClient();
 		try 
 		{
-			HttpClient client = new DefaultHttpClient();
-			HttpPost post = new HttpPost(url);
-			HttpResponse response = client.execute(post);
-			HttpEntity entity = response.getEntity();
-			//if(entity != null) return EntityUtils.toString(entity);
-			//else return "NO Entity !!";
-			
-			input = new InputStreamReader(entity.getContent());
-			reafBuf = new BufferedReader(input);
-			String str1 = null;
-			while(reafBuf.readLine() != null)
+			client.getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 10000);
+			request = new HttpPost(url);
+			response = client.execute(request);
+			if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) 
 			{
-				str1 = reafBuf.readLine();
+				String str1 = EntityUtils.toString(response.getEntity());
+				try
+				{
+					str2 = new JSONObject(str1).getString("message");
+				}
+				catch(JSONException e)
+				{
+					mesg.setText("JSONException Occurred. " + e.getMessage());
+					e.printStackTrace();
+				}	
+				
+				mesg.setText(str2);
 			}
-			reafBuf.close();
-			return str1;
-		} catch (Exception e) 
+		} 
+		catch(ClientProtocolException e)
 		{
+			mesg.setText("ClientProtocolException Ocurred. ");
 			e.printStackTrace();
-			return "Other Exception Occurred.";
+		}
+		catch (IOException e) 
+		{
+			// TODO: handle exception
+			mesg.setText("IOException Occurred.\n" + e.getMessage());
+			e.printStackTrace();
+		}
+		catch (Exception e) 
+		{
+			// TODO: handle exception
+			mesg.setText("Other Exception Occurred.");
+			e.printStackTrace();
 		}
 	}
 }
